3a7528fa01c84c67567c39d20189c13ff0be049f
Yadiaau <wheredoesthesunrise@gmail.com>
2025-12-18 20:10:06 +0100

    i created a croissant and a hearty butty button pink and dark pink and i said the the heart is dark te amo papi <3

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
 import tkinter as tk
 from tkinter import filedialog
 import os
+
+class Croissant: 
+    def __init__(self, root): 
+        self.root=root 
+        self.root.title ("I love you couter") 
+        self.root.geometry ("800x600")
+
+        self.count
+        self.timer 
+        self.heart_color ="#FFD1DC" 
+        self.dark_color = "#F8B2C1"
+        self.is_dark = False
+        self.bg_photo = None
+         
