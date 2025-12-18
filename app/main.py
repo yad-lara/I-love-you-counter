0e83d18a3f8fac0caabcce07806089568772e394
Yadiaau <wheredoesthesunrise@gmail.com>
2025-12-18 21:06:13 +0100

    i created heart for dada

--- app/main.py
+++ app/main.py
@@ -19,6 +19,14 @@ class Croissant:
         self.canvas.pack(fill=tk.BOTH, expand=True)
 
         self.counter_label = tk.Label(self.canvas, text="0", font=("Arial", 48, "bold"), bg="white", fg="#FF1493")
+        self.counter_window = self.canvas.create_window(0,0, window = self.counter_label)
+
+        self.heart_canvas = tk.Canvas(self.canvas, width= 200, height= 200, bg = "white", highlightthickness=0)
+        self.heart_window = self.canvas.create_window(0,0, window=self.heart_canvas)
+
+        self.heart = self.heart_canvas.create_oval(50, 80, 150, 180, fill = self.heart_color, outline="")
+        self.heart_text=""
+        self.heart_canvas.create_text(100, 130, text="", font=("Arial", 16, "bold"), fill="white")
         
 
          
