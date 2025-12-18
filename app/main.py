5cc76f645bb3d73dd486f8396ecd58265a61c2a5
yadiel-lara <yadiel.lara@upr.edu>
2025-12-18 20:26:37 +0100

    Created canvas, created counter label

--- app/main.py
+++ app/main.py
@@ -14,4 +14,11 @@ class Croissant:
         self.dark_color = "#F8B2C1"
         self.is_dark = False
         self.bg_photo = None
+
+        self.canvas = tk.Canvas(root, highlightthickness=0, bg="white")
+        self.canvas.pack(fill=tk.BOTH, expand=True)
+
+        self.counter_label = tk.Label(self.canvas, text="0", font=("Arial", 48, "bold"), bg="white", fg="#FF1493")
+        
+
          
